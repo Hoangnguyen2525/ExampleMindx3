a745ecc6fafe8a0233c85ddbdb5d5544b6293cd1
Hoangnguyen2525 <99356519+Hoangnguyen2525@users.noreply.github.com>
2022-04-13 16:19:52 +0700

    Add files via upload

---
+++ test cuối khóa CI/index.js
@@ -0,0 +1,27 @@
+//bài 1//
+function adjacentElementsProduct(arr) {
+    return arr.slice(0, -1)
+              .reduce((max, n, i) => Math.max(max, n * arr[i + 1]), -Infinity)
+  }
+  
+  
+  console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+  // bài 2 //
+  function alternatingSums(inputArray) {
+    alternatingSums([60, 40, 55, 75, 64])
+            var sumsArr;  var sumTeam1 = 0;
+            var sumTeam2 = 0;
+            for (var i = 0; i < inputArray.length; i++) {
+                if (i%2 == 0) {
+                  sumTeam1 = sumTeam1 + inputArray[i]
+                } else {
+                  sumTeam2 = sumTeam2 + inputArray[i]
+                }
+            }
+            sumsArr.push(sumTeam1);
+            sumsArr.push(sumTeam2);
+            return sumsArr
+          }
+            var mindX2 =  alternatingSums([60, 40, 55, 75, 64])
+            console.log(mindx2)
